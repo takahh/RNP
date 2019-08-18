9dda421ecafdf8e931c4565f76929a09751d50a1
Takayuki Kimura <Takayuki.Kimura@schrodinger.com>
2019-08-18 10:35:51 +0900

    improved in generating .hb2 files

--- python/hbond_extraction/chimera/findHbond_mp.py
+++ python/hbond_extraction/chimera/findHbond_mp.py
@@ -1,21 +1,114 @@
 # ----------------------------------------------------------
 # First I ran find_hbond.py but was very slow because of
 # the time for Chimera to read cif files
-# so made this code to expedite
+# so made this code to expedite it
 # ----------------------------------------------------------
 
 # ----------------------------------------------------------
 # import
 # ----------------------------------------------------------
+from multiprocessing import Process
+import os
+from chimera import runCommand as rc
 
 # ----------------------------------------------------------
 # constants
 # ----------------------------------------------------------
+path = '/Users/tkimura/Desktop/RNP/'
+cif_path = path + 'downloads_PDB/'
+pdb_path = path + 'check_contact/PDBfiles/'
+hbout_path = path + 'check_contact/chimera_hb_out/'
 
 # ----------------------------------------------------------
 # functions
 # ----------------------------------------------------------
+# ----------------------------------------------------------
+# make a done-list
+# ----------------------------------------------------------
+def done_list():
+	done_list = []
+	for xprm in ('xray_cif/', 'EM_cif/'):
+		for ciffile in os.listdir(hbout_path + xprm):
+			done_list.append(ciffile[0:4])
+	return done_list
+
+
+# ----------------------------------------------------------
+# make a pdb file list
+# ----------------------------------------------------------
+def pdb_list():
+	pdb_list = []
+	for xprm in ('xray/', 'EM/'):
+		for pdbfile in os.listdir(pdb_path + xprm):
+			pdb_list.append(pdbfile[0:4])
+	return pdb_list
+
+
+# ----------------------------------------------------------
+# run chimera
+# ----------------------------------------------------------
+def hfind(folder_path, done_list, pdb_list, hbout_path):
+	# run let_chimera_hfind.py for the folder_path
+	# after checking completed files
+	for ciffile in os.listdir(folder_path):
+		if '.cif' not in ciffile or ciffile[0:4] in done_list:
+			continue
+		if ciffile[0:4] + '.pdb' in pdb_list:
+			rc("open " + pdb_path + ciffile[0:4] + '.pdb')
+		else:
+			rc("open " + folder_path + ciffile)
+		output_file = hbout_path + ciffile.replace('cif', 'hb2')
+		# ----------------------------------------------------------
+		# create copies in 5A around #0, find hbonds, and then
+		# (later) choose bonds including #0
+		# ----------------------------------------------------------
+		rc("cryst #0 5 copies true") # create copies in 5A around model #0
+		rc("hbonds showDist true intraMol false intraRes false saveFile " + output_file)
+		rc("close all")
+		# print('#######################' + ciffile + ' is DONE!!!')
+
+
+def func_1(xprm):
+	folder_path = cif_path + xprm + '/1/'
+	hfind(folder_path, done_list, pdb_list, hbout_path + xprm)
+
+def func_2(xprm):
+	folder_path = cif_path + xprm + '/2/'
+	hfind(folder_path, done_list, pdb_list, hbout_path + xprm)
+
+def func_3(xprm):
+	folder_path = cif_path + xprm + '/3/'
+	hfind(folder_path, done_list, pdb_list, hbout_path + xprm)
+
+def func_4(xprm):
+	folder_path = cif_path + xprm + '/4/'
+	hfind(folder_path, done_list, pdb_list, hbout_path + xprm)
+
+def func_5(xprm):
+	folder_path = cif_path + xprm + '/5/'
+	hfind(folder_path, done_list, pdb_list, hbout_path + xprm)
+
+def func_6(xprm):
+	folder_path = cif_path + xprm + '/6/'
+	hfind(folder_path, done_list, pdb_list, hbout_path + xprm)
 
 # ----------------------------------------------------------
 # main
 # ----------------------------------------------------------
+done_list = done_list()
+pdb_list = pdb_list()
+
+for xprm in ('xray_cif/', 'EM_cif/'):
+
+	p1 = Process(target=func_1, args=(xprm,))
+	p1.start()
+	p2 = Process(target=func_2, args=(xprm,))
+	p2.start()
+	p3 = Process(target=func_3, args=(xprm,))
+	p3.start()
+	p4 = Process(target=func_4, args=(xprm,))
+	p4.start()
+	p5 = Process(target=func_5, args=(xprm,))
+	p5.start()
+	p6 = Process(target=func_6, args=(xprm,))
+	p6.start()
--- python/hbond_extraction/chimera/let_chimera_hfind.py
+++ python/hbond_extraction/chimera/let_chimera_hfind.py
@@ -19,6 +19,8 @@ cif_path = path + 'downloads_PDB/'
 pdb_path = path + 'check_contact/PDBfiles/'
 hbout_path = path + 'check_contact/chimera_hb_out/'
 done_list,pdb_list = [], []
+file_list_dic = {}
+skip_list = ['4uyj', '6bbo','6b0b','1xpu']
 
 # ----------------------------------------------------------
 # make a done-list
@@ -34,25 +36,47 @@ for xprm in ('xray/', 'EM/'):
 	for pdbfile in os.listdir(pdb_path + xprm):
 		pdb_list.append(pdbfile[0:4])
 
+print(pdb_list)
+
+# ----------------------------------------------------------
+# make a pdb file list
+# ----------------------------------------------------------
+def sorted_cif_list():
+	for exp in ('xray', 'EM'):
+		all_files = (os.path.join(basedir, filename) for basedir,\
+					dirs, files in os.walk(cif_path + exp + '_cif/')\
+					 for filename in files)
+		sorted_files = sorted(all_files, key=os.path.getsize)
+		file_list_dic[exp] = sorted_files
+	return file_list_dic
+
 # ----------------------------------------------------------
 # main
 # ----------------------------------------------------------
 os.chdir(path)
 for xprm in ('xray_cif/', 'EM_cif/'):
-	for ciffile in os.listdir(cif_path + xprm):
-		if '.cif' not in ciffile or ciffile[0:4] in done_list:
+	cif_list_dic = sorted_cif_list()
+	for ciffile in cif_list_dic[xprm[:-5]]:
+		cif_filename = ciffile.split('/')[-1]
+		if cif_filename[0:4] in skip_list: # skip erroneous entry
 			continue
-		if ciffile[0:4] + '.pdb' in pdb_list:
-			rc("open " + pdb_path + xprm + ciffile[0:4] + '.pdb')
-			print(ciffile + ' :READING PDB FILE INSTEAD !!!!')
+		if '.cif' not in ciffile or cif_filename[0:4] in done_list:
+			continue
+		if cif_filename[0:4] in pdb_list:
+			print("open " + pdb_path + xprm[:-5] + '/' + cif_filename[0:4] + '.pdb')
+			rc("open " + pdb_path + xprm[:-5] + cif_filename[0:4] + '.pdb')
+			print(cif_filename + ' :READING PDB FILE INSTEAD !!!!')
 		else:
-			print(ciffile + ' :READING CIF FILE !!!!')
-			rc("open " + cif_path + xprm + ciffile)
-		output_file = hbout_path + xprm + ciffile.replace('cif', 'hb2')
+			print(cif_filename + ' :READING CIF FILE !!!!')
+			rc("open " + ciffile)
+		output_file = hbout_path + xprm + cif_filename.replace('cif', 'hb2')
 		# ----------------------------------------------------------
 		# create copies in 5A around #0, find hbonds, and then
 		# (later) choose bonds including #0
 		# ----------------------------------------------------------
-		rc("cryst #0 5 copies true") # create copies in 5A around model #0
-		rc("hbonds showDist true intraMol false intraRes false saveFile " + output_file)
-		rc("close all")
+		try:
+			rc("cryst #0 5 copies true") # create copies in 5A around model #0
+			rc("hbonds showDist true intraMol false intraRes false saveFile " + output_file)
+			rc("close all")
+		except ValueError:
+			print('ValueError!!!!')
